584d40b3a035d17d7f86c89357ff48cf2fa1c606
ramachandra <ramchandra081@gmail.com>
2024-08-29 12:49:34 +1200

    variables

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " Ramesh:: Hi suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I'm good thank you. How about you?"
+echo " Ramesh:: I am doing fine Suresh. what's going on?"
+echo " Suresh:: I started learning shell scrpiting "
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh 
+
+echo " $Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1, I'm good thank you. How about you?"
+echo " $Person1:: I am doing fine $Person2. what's going on?"
+echo " $Person2:: I started learning shell scrpiting "
